f7c463ba239c9c4d2226751f699f80e9913030d6
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2023-09-13 04:23:11 +0900

    add --target web at build.

--- code_editor_wasm.d.ts
+++ code_editor_wasm.d.ts
@@ -116,3 +116,48 @@ export class CodeEditorWorkspace {
 export class Param {
   free(): void;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly wasm_start: () => void;
+  readonly get_unsaved_action_queue: (a: number) => void;
+  readonly undone: () => number;
+  readonly redo: () => number;
+  readonly __wbg_param_free: (a: number) => void;
+  readonly deserialize_param: (a: number, b: number, c: number) => void;
+  readonly __wbg_codeeditorworkspace_free: (a: number) => void;
+  readonly codeeditorworkspace_new: () => number;
+  readonly codeeditorworkspace_generate_component_code: (a: number, b: number, c: number, d: number) => void;
+  readonly codeeditorworkspace_serialize_component: (a: number, b: number, c: number, d: number) => void;
+  readonly codeeditorworkspace_load_component: (a: number, b: number) => void;
+  readonly codeeditorworkspace_execute: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
